5c782fd67bddf1d7e94a8d5e2976b1d56c5e0475
Isti <1stone.seres@gmail.com>
2021-03-24 15:27:02 +0100

    Update OddNumbers.java

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,15 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+
+        var ref = new Object() {
+            Integer sum = 0;
+        };
+
+        numbers.stream()
+                .filter(number -> number%2==1 || number%2==-1)
+                .forEach(number -> ref.sum += number);
+
+        return ref.sum;
     }
 }
